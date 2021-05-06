8a0401ec9d2214be2db32b36b3bf1295430bcc3b
Shinoj <ShinojShayin@users.noreply.github.com>
2021-05-06 15:06:50 +0530

    Exception handler controller added for all exceptions in the system

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.upgrad.FoodOrderingApp.api.exception;
 
 import com.upgrad.FoodOrderingApp.api.model.ErrorResponse;
-import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
-import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
-import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
-import com.upgrad.FoodOrderingApp.service.exception.UpdateCustomerException;
+import com.upgrad.FoodOrderingApp.service.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,7 +22,8 @@ public class RestExceptionHandler {
      * @return ErrorResponse
      */
     @ExceptionHandler(SignUpRestrictedException.class)
-    public ResponseEntity<ErrorResponse> signUpRestrictedException (SignUpRestrictedException exception, WebRequest request){
+    public ResponseEntity<ErrorResponse> signUpRestrictedException (SignUpRestrictedException exception,
+                                                                    WebRequest request){
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
                 .code(exception.getCode())
                 .message(exception.getErrorMessage())
@@ -41,7 +39,8 @@ public class RestExceptionHandler {
      * @return ErrorResponse
      */
     @ExceptionHandler(AuthenticationFailedException.class)
-    public ResponseEntity<ErrorResponse> authenticationFailedException (AuthenticationFailedException exception, WebRequest request){
+    public ResponseEntity<ErrorResponse> authenticationFailedException (AuthenticationFailedException exception,
+                                                                        WebRequest request){
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
                 .code(exception.getCode())
                 .message(exception.getErrorMessage())
@@ -57,7 +56,8 @@ public class RestExceptionHandler {
      * @return ErrorResponse
      */
     @ExceptionHandler(AuthorizationFailedException.class)
-    public ResponseEntity<ErrorResponse> authorizationFailedException (AuthorizationFailedException exception, WebRequest request){
+    public ResponseEntity<ErrorResponse> authorizationFailedException (AuthorizationFailedException exception,
+                                                                       WebRequest request){
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
                 .code(exception.getCode())
                 .message(exception.getErrorMessage())
@@ -74,7 +74,144 @@ public class RestExceptionHandler {
      * @return ErrorResponse
      */
     @ExceptionHandler(UpdateCustomerException.class)
-    public ResponseEntity<ErrorResponse> updateCustomerException (UpdateCustomerException exception, WebRequest request){
+    public ResponseEntity<ErrorResponse> updateCustomerException (UpdateCustomerException exception,
+                                                                  WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * This method is invoked when AddressNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(AddressNotFoundException.class)
+    public ResponseEntity<ErrorResponse> addressNotFoundException (AddressNotFoundException exception,
+                                                                   WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when CategoryNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ErrorResponse> categoryNotFoundException (CategoryNotFoundException exception,
+                                                                   WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when CouponNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(CouponNotFoundException.class)
+    public ResponseEntity<ErrorResponse> couponNotFoundException (CouponNotFoundException exception,
+                                                                   WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when InvalidRatingException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(InvalidRatingException.class)
+    public ResponseEntity<ErrorResponse> invalidRatingException (InvalidRatingException exception,
+                                                                  WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * This method is invoked when ItemNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<ErrorResponse> itemNotFoundException (ItemNotFoundException exception,
+                                                                 WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when PaymentMethodNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(PaymentMethodNotFoundException.class)
+    public ResponseEntity<ErrorResponse> paymentMethodNotFoundException (PaymentMethodNotFoundException exception,
+                                                                WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when RestaurantNotFoundException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    public ResponseEntity<ErrorResponse> restaurantNotFoundException (RestaurantNotFoundException exception,
+                                                                         WebRequest request){
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+                .code(exception.getCode())
+                .message(exception.getErrorMessage())
+                .rootCause(getClassName(exception.toString())),
+                HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * This method is invoked when SaveAddressException thrown and relevant error code and error message
+     *
+     * @param exception
+     * @param request
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(SaveAddressException.class)
+    public ResponseEntity<ErrorResponse> saveAddressException (SaveAddressException exception,
+                                                                      WebRequest request){
         return new ResponseEntity<ErrorResponse>(new ErrorResponse()
                 .code(exception.getCode())
                 .message(exception.getErrorMessage())
