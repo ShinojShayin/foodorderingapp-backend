1788e4bb791a959974b0d7a33fa9502ada44f210
Shinoj <ShinojShayin@users.noreply.github.com>
2021-05-08 19:56:55 +0530

    search restaurant by name added

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -10,14 +10,12 @@ import com.upgrad.FoodOrderingApp.service.businness.ItemService;
 import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.LinkedList;
@@ -79,6 +77,53 @@ public class RestaurantController {
 
         RestaurantListResponse restaurantListResponse = new RestaurantListResponse().restaurants(restaurantLists);
         return new ResponseEntity<RestaurantListResponse>(restaurantListResponse, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET,path = "/name/{restaurant_name}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<RestaurantListResponse> getRestaurantByName (
+            @PathVariable(value = "restaurant_name") final String restaurantName)
+            throws RestaurantNotFoundException {
+
+        List<RestaurantEntity> restaurantEntities = restaurantService.restaurantsByName(restaurantName);
+
+        if(!restaurantEntities.isEmpty()){
+            List<RestaurantList> restaurantLists = new LinkedList<>();
+
+            restaurantEntities.stream().forEach(restaurantEntity -> {
+
+                RestaurantDetailsResponseAddressState restaurantDetailsResponseAddressState = new RestaurantDetailsResponseAddressState()
+                        .id(UUID.fromString(restaurantEntity.getAddress().getState().getUuid()))
+                        .stateName(restaurantEntity.getAddress().getState().getStateName());
+
+
+                RestaurantDetailsResponseAddress restaurantDetailsResponseAddress = new RestaurantDetailsResponseAddress()
+                        .id(UUID.fromString(restaurantEntity.getAddress().getUuid()))
+                        .city(restaurantEntity.getAddress().getCity())
+                        .flatBuildingName(restaurantEntity.getAddress().getFlatBuilNo())
+                        .locality(restaurantEntity.getAddress().getLocality())
+                        .pincode(restaurantEntity.getAddress().getPincode())
+                        .state(restaurantDetailsResponseAddressState);
+
+                List<CategoryEntity> categoryEntities = categoryService.getCategoriesByRestaurant(restaurantEntity.getUuid());
+                RestaurantList restaurantList = new RestaurantList()
+                        .id(UUID.fromString(restaurantEntity.getUuid()))
+                        .restaurantName(restaurantEntity.getRestaurantName())
+                        .averagePrice(restaurantEntity.getAvgPrice())
+                        .categories(categoryService.getCategoryAsString(categoryEntities))
+                        .customerRating(BigDecimal.valueOf(restaurantEntity.getCustomerRating()))
+                        .numberCustomersRated(restaurantEntity.getNumberCustomersRated())
+                        .photoURL(restaurantEntity.getPhotoUrl())
+                        .address(restaurantDetailsResponseAddress);
+
+                restaurantLists.add(restaurantList);
+            });
+
+            RestaurantListResponse restaurantListResponse = new RestaurantListResponse().restaurants(restaurantLists);
+            return new ResponseEntity<RestaurantListResponse>(restaurantListResponse, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<RestaurantListResponse>(new RestaurantListResponse(),HttpStatus.OK);
+        }
     }
 
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -1,7 +1,10 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.common.RestaurantNotFoundErrorCode;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,15 @@ public class RestaurantService {
 
     public List<RestaurantEntity> getRestaurantOrderByRating() {
         List<RestaurantEntity> restaurantEntities = restaurantDao.getRestaurantOrderByRating();
+        return restaurantEntities;
+    }
+
+    public List<RestaurantEntity> restaurantsByName(String restaurantName) throws RestaurantNotFoundException {
+
+        if(StringUtils.isEmpty(restaurantName))
+            throw new RestaurantNotFoundException(RestaurantNotFoundErrorCode.RNF_003);
+
+        List<RestaurantEntity> restaurantEntities = restaurantDao.restaurantsByName(restaurantName);
         return restaurantEntities;
     }
 }
