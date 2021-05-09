7d19ba4423e0b304ed9d6d1fe7535898fc3350f4
Shinoj <ShinojShayin@users.noreply.github.com>
2021-05-09 18:28:49 +0530

    Added some endpoint to restaurant controller

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -1,7 +1,52 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.CategoryItemDao;
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantItemDao;
+import com.upgrad.FoodOrderingApp.service.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    RestaurantDao restaurantDao;
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    RestaurantItemDao restaurantItemDao;
+
+    @Autowired
+    CategoryItemDao categoryItemDao;
+
+
+    public List<ItemEntity> getItemsByCategoryAndRestaurant(String restaurantUuid, String uuid) {
+
+        RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUuid(restaurantUuid);
+
+        CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(uuid);
+
+        List<RestaurantItemEntity> restaurantItemEntities = restaurantItemDao.getItemsByRestaurant(restaurantEntity);
+
+        List<CategoryItemEntity> categoryItemEntities = categoryItemDao.getItemsByCategory(categoryEntity);
+
+        List<ItemEntity> itemEntities = new LinkedList<>();
+
+        restaurantItemEntities.forEach(restaurantItemEntity -> {
+            categoryItemEntities.forEach(categoryItemEntity -> {
+                if(restaurantItemEntity.getItem().equals(categoryItemEntity.getItem())){
+                    itemEntities.add(restaurantItemEntity.getItem());
+                }
+            });
+        });
+
+        return itemEntities;
+    }
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -1,14 +1,28 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.common.CategoryNotFoundErrorCode;
+import com.upgrad.FoodOrderingApp.service.common.InvalidRatingErrorCode;
 import com.upgrad.FoodOrderingApp.service.common.RestaurantNotFoundErrorCode;
+import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantCategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantCategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.InvalidRatingException;
 import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class RestaurantService {
@@ -16,7 +30,13 @@ public class RestaurantService {
     @Autowired
     RestaurantDao restaurantDao;
 
-    public List<RestaurantEntity> getRestaurantOrderByRating() {
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    RestaurantCategoryDao restaurantCategoryDao;
+
+    public List<RestaurantEntity> restaurantsByRating() {
         List<RestaurantEntity> restaurantEntities = restaurantDao.getRestaurantOrderByRating();
         return restaurantEntities;
     }
@@ -28,5 +48,68 @@ public class RestaurantService {
 
         List<RestaurantEntity> restaurantEntities = restaurantDao.restaurantsByName(restaurantName);
         return restaurantEntities;
+    }
+
+    public List<RestaurantEntity> restaurantByCategory(String categoryId) throws CategoryNotFoundException {
+
+        if(StringUtils.isEmpty(categoryId))
+            throw new CategoryNotFoundException(CategoryNotFoundErrorCode.CNF_001);
+
+        CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(categoryId);
+
+        if(Objects.isNull(categoryEntity))
+            throw new CategoryNotFoundException(CategoryNotFoundErrorCode.CNF_002);
+
+        List<RestaurantCategoryEntity> restaurantCategoryEntities = restaurantCategoryDao.getRestaurantByCategory(categoryEntity);
+
+        List<RestaurantEntity> restaurantEntities = new LinkedList<>();
+        restaurantCategoryEntities.forEach(restaurantCategoryEntity -> {
+            restaurantEntities.add(restaurantCategoryEntity.getRestaurant());
+        });
+
+        return restaurantEntities;
+
+    }
+
+    public RestaurantEntity restaurantByUUID(String restaurantUuid) throws RestaurantNotFoundException {
+
+        if(StringUtils.isEmpty(restaurantUuid))
+            throw new RestaurantNotFoundException(RestaurantNotFoundErrorCode.RNF_002);
+
+        RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUuid(restaurantUuid);
+
+        if (Objects.isNull(restaurantEntity))
+            throw new RestaurantNotFoundException(RestaurantNotFoundErrorCode.RNF_001);
+
+        return restaurantEntity;
+    }
+
+
+
+    Predicate<Double> isValidCustomerRating = (rating) ->{
+        if(Objects.nonNull(rating)){
+            return rating >= 1.0 && rating <= 5.0 ;
+        }
+        return false;
+    };
+
+    public RestaurantEntity updateRestaurantRating(RestaurantEntity restaurantEntity, Double customerRatingProvided)
+            throws InvalidRatingException {
+
+        if(!isValidCustomerRating.test(customerRatingProvided))
+            throw new InvalidRatingException(InvalidRatingErrorCode.IRE_001);
+
+        double customerRating = restaurantEntity.getCustomerRating();
+        Integer numberCustomersRated = restaurantEntity.getNumberCustomersRated();
+
+        double calculatedRating = ( (customerRating * numberCustomersRated.doubleValue())
+                + customerRatingProvided ) / restaurantEntity.getNumberCustomersRated();
+
+        DecimalFormat format = new DecimalFormat("##.0");
+        restaurantEntity.setCustomerRating(Double.parseDouble(format.format(calculatedRating)));
+
+        RestaurantEntity restaurantEntityResp = restaurantDao.updateRestaurantRating(restaurantEntity);
+
+        return restaurantEntityResp;
     }
 }
